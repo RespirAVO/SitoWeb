03477d4fd77b6efaec751d86f5bea13aaa763e50
Ant00o <belliniantonio205@gmail.com>
2022-05-10 21:51:46 +0200

    Modifiche alle mappe e moltre altre modifiche
    Co-authored-by: Marco Schiavello <MarcoSchiavello@users.noreply.github.com>

--- js/map.js
+++ js/map.js
@@ -1,4 +1,17 @@
 import Icons from "./icons.js";
+import Utils from "./utils.js";
+
+const forbiddenFields = ['timestamp', 'latitude', 'longitude', 'latitudine', 'longitudine'];  
+
+const optsList = document.querySelector('#optionList');
+const fileList = document.querySelector('#fileList');
+
+const limitsInGm3 = {
+    'PM10': 50,
+    'PM25': 25,
+    'O3': 120,
+    'NO2': 40
+};
 
 const map = L.map('mappaR', {
     center: [0, 0], // posizionamento iniziale 
@@ -14,26 +27,62 @@ L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     zoomOffset: -1,
 }).addTo(map);
 
+const markers = L.layerGroup().addTo(map);
 
-$.get('../data.csv', csvString => {
+function drawMarkers(map, data, field) {
+    markers.clearLayers();
 
-    // Papa parse libreria per parsare file CSV e JSON https://www.papaparse.com/
-    let data = Papa.parse(csvString, { header: true, dynamicTyping: true }).data;
-
-    map.panTo([data[0].latitude, data[0].longitude]);// riposiziona il riferimento centrale della mappa al primo punto
+    map.panTo([data[0].latitude === undefined ? data[0].latitudine : data[0].latitude, data[0].longitude === undefined ? data[0].longitudine : data[0].longitude]);// riposiziona il riferimento centrale della mappa al primo punto
+    document.querySelector('#optionList');
+    const trimmedField = field.replace('(ppb)','').replace('(ug/m3)','').trim();
+    const limit = field.indexOf('(ppb)') !== -1 ? Utils.gm3ToPpb(limitsInGm3[trimmedField], trimmedField) : limitsInGm3[trimmedField];
 
     for (const row of data) {
-
+        let lat = row.latitude === undefined ? row.latitudine : row.latitude;
+        let lon = row.longitude === undefined ? row.longitudine : row.longitude;
         let popupMsg = '';
 
         for (const key of Object.keys(row))
             popupMsg += key + ': ' + row[key] + '<br>';
 
-        const marker = L.marker([row.latitude, row.longitude], {
-            icon: (row['superato limite'].split(' ')[0] === 'SI' ? Icons.red : Icons.green),
+        const marker = L.marker([lat, lon], {
+            icon: (row[field] > (limit * 2) ? Icons.violet : 
+                   row[field] > limit ? Icons.red : 
+                   row[field] > (limit - (limit / 4)) ? Icons.orange : 
+                   row[field] > (limit / 2) ? Icons.yellow : 
+                   Icons.green),
             opacity: 1
-        }).addTo(map);
+        }).addTo(markers);
 
         marker.bindPopup(popupMsg);
     }
+}
+
+async function uploadMesuresFromFile(map, fileName) {
+    markers.clearLayers();
+    const data = await Utils.csvToObj(fileName);
+
+    optsList.innerHTML = '';
+    for (const key of Object.keys(data[0])) {
+        if(forbiddenFields.indexOf(key) !== -1)
+            continue;
+        
+        const newEle = document.createElement('option');
+        newEle.setAttribute('value', key);
+        newEle.innerText = key;
+        optsList.appendChild(newEle);
+         
+    }
+
+    drawMarkers(map, data, document.querySelector('#optionList').children[0].value);
+
+    optsList.addEventListener('change', e => {
+        drawMarkers(map, data, optsList.value);
+    });
+}
+
+uploadMesuresFromFile(map, fileList.value);
+
+fileList.addEventListener('change', e => {
+    uploadMesuresFromFile(map, fileList.value);
 });
